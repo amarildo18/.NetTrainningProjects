4f2db749066acfa24418986ab18667a777e54186
amarildo.ferreira@minfin.gov.ao <amarildo.ferreira@minfin.gov.ao>
2024-02-20 12:52:32 +0100

    71. 0707 Mapeando as Entidades do Banco de Dados com Entity Framework

---
+++ GeekShopping/GeekShopping.ProductAPI/Models/Context/SqlServerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Models.Context
+{
+    public class SqlServerContext : DbContext
+    {
+        protected SqlServerContext()
+        {
+        }
+
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options){ }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductAPI.Models.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// configurando a string de conexão
+var connection = builder.Configuration["ConnectionStrings:SqlServerConnection"];
+builder.Services.AddDbContext <SqlServerContext> (option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString(connection)));
 
 var app = builder.Build();
 
