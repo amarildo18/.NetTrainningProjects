170b2104cfd0b3ba7cc8b1627033fada1e2d0c53
amarildo.ferreira@minfin.gov.ao <amarildo.ferreira@minfin.gov.ao>
2024-02-22 19:43:41 +0100

    77. 0803 Criação dos VO's do Microsserviço de Produtos

---
+++ GeekShopping/GeekShopping.ProductAPI/Data/ValueObjects/ProductVO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeekShopping.ProductAPI.Data.ValueObjects
+{
+    public class ProductVO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public decimal? PricePrice { get; set; }
+        public string? ImageURL { get; set; }
+
+    }
+}
