0bf1791917079bc2ee5ed787fd052e7e7ffdd5a1
amarildo.ferreira@minfin.gov.ao <amarildo.ferreira@minfin.gov.ao>
2024-02-25 08:50:26 +0100

    91. 0904 Entendendo e Implementando a Classe HttpClientExtensions

---
+++ GeekShopping/GeekShopping.web/Utils/HttpClientExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace GeekShopping.web.Utils
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAsync<T>(this HttpResponseMessage response)
+        {
+            if(response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
+            }
+
+            var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true});
+        }
+    }
+}
