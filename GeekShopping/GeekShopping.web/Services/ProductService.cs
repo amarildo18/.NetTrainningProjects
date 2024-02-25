122d61651cb25a0e19236c8064b1c2a3a36d9dad
amarildo.ferreira@minfin.gov.ao <amarildo.ferreira@minfin.gov.ao>
2024-02-25 17:51:51 +0100

    93. 0906 Implementando a Classe ProductService

---
+++ GeekShopping/GeekShopping.web/Services/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using GeekShopping.web.Models;
+using GeekShopping.web.Services.IServices;
+using GeekShopping.web.Utils;
+
+namespace GeekShopping.web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _Client;
+        public const string basePath = "api/v1/product";
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _Client.GetAsync(basePath);
+            return await response.ReadContentAsync<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await _Client.GetAsync($"{basePath}/{id}");
+            return await response.ReadContentAsync<ProductModel>();
+        }
+
+        public async Task<ProductModel> CreateProduct(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductModel> UpdateProduct(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteProductById(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
