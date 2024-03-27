06af35c9b4ea90a2d791096f484a998496c7e8c2
amarildo.ferreira@minfin.gov.ao <amarildo.ferreira@minfin.gov.ao>
2024-03-27 14:36:38 +0100

    113. 1108 Adição do DbContext ao Identity Server

---
+++ GeekShopping/GeekShopping.IdentityServer/DataAccess/Context/SqlServerContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.DataAccess.Context
+{
+    public class SqlServerContext: IdentityDbContext<ApplicationUser>
+    {
+        public SqlServerContext(DbContextOptions<SqlServerContext> options) : base(options) { }
+    }
+}
